0de5722760134914c7c74a72d51e6fc6a76c75f8
sunnybear <sunnybear@foxmail.com>
2016-12-08 21:27:52 +0800

    嵌套滑动

--- app/src/main/java/com/sunnybear/rxandroid/presenter/DesignActivity.java
+++ app/src/main/java/com/sunnybear/rxandroid/presenter/DesignActivity.java
@@ -5,22 +5,22 @@ import android.support.annotation.Nullable;
 
 import com.sunnybear.library.basic.presenter.Presenter;
 import com.sunnybear.library.basic.presenter.PresenterActivity;
-import com.sunnybear.rxandroid.view.DesignViewBinder;
+import com.sunnybear.rxandroid.view.CoordinatorViewBinder;
 
 /**
  * Created by chenkai.gu on 2016/12/8.
  */
-public class DesignActivity extends PresenterActivity<DesignViewBinder> {
+public class DesignActivity extends PresenterActivity<CoordinatorViewBinder> {
 
-    @Override
+    /*@Override
     protected DesignViewBinder getViewBinder(Presenter presenter) {
         return new DesignViewBinder(presenter);
-    }
+    }*/
 
-//    @Override
-//    protected CoordinatorViewBinder getViewBinder(Presenter presenter) {
-//        return new CoordinatorViewBinder(presenter);
-//    }
+    @Override
+    protected CoordinatorViewBinder getViewBinder(Presenter presenter) {
+        return new CoordinatorViewBinder(presenter);
+    }
 
     @Override
     protected void onViewBindFinish(@Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/sunnybear/rxandroid/view/CoordinatorViewBinder.java
+++ app/src/main/java/com/sunnybear/rxandroid/view/CoordinatorViewBinder.java
@@ -1,14 +1,20 @@
 package com.sunnybear.rxandroid.view;
 
+import android.support.v4.widget.NestedScrollView;
+
 import com.sunnybear.library.basic.presenter.Presenter;
 import com.sunnybear.library.basic.view.ViewBinder;
 import com.sunnybear.rxandroid.R;
 import com.sunnybear.rxandroid.presenter.DesignActivity;
 
+import butterknife.Bind;
+
 /**
  * Created by chenkai.gu on 2016/12/8.
  */
 public class CoordinatorViewBinder extends ViewBinder<DesignActivity> {
+    @Bind(R.id.scroll)
+    NestedScrollView mScroll;
 
     public CoordinatorViewBinder(Presenter presenter) {
         super(presenter);
